13621e23ca1e5184fbfb5dd15beda28f2015baf8
Roman Kopylov <roman.kopylov@eurochem.ru>
2019-04-17 11:09:25 +0300

    Use js file from root directory

--- sample/server.js
+++ sample/server.js
@@ -1,8 +1,10 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '..')));
 
 app.post('/submit', (req, res) => res.json('success'));
 
